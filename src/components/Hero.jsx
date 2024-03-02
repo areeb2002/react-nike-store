58795410a1f914d932dc2c506488a8b1be06b4f0
areeb hussain <areebhussain1974@gmail.com>
2024-03-02 18:35:15 +0530

    Page 1

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+const Herosection=()=>{
+    return(
+        <main className="hero container">
+            <div className="herocontent">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button className="secondary-btn">Category</button>
+                </div>
+                <div className="shopping">
+                    <p>Also Available On</p>
+                    <div className="brand-name">
+                        <img src="./images/amazon.png" alt="Amazon-logo" />
+                        <img src="./images/flipkart.png" alt="Flipkart-logo" />
+                    </div>
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src="./images/shoe_image.png" alt="hero-image" />
+            </div>
+        </main>
+    );
+};
+export default Herosection;
